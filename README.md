36737514acaf304f2ff0911600a88386501c164f
imssp <imssp25@gmail.com>
2021-06-23 12:19:24 +0530

    Day-1

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # DevSecOps
+
+
+### Agenda
+
+* Day-1
+
+1)	AWS Setup (Login Console and CLI)
+2)	Terraform (Overview)
+3)	Creation On Infra on AWS Using Terraform
+4)	Test and deactivate everything.
---
+++ Terraform/dev/vpc/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-azeem"
+  cidr = "192.168.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["192.168.1.0/24"]
+  public_subnets  = ["192.168.100.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+  instance_tenancy = "default"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
